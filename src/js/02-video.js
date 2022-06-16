48f9cdc566529b6ca629593c5311aa51191d15ff
pchomiak <pchomiak@gmail.com>
2022-06-16 22:19:39 +0200

    Update 02-video.js
    .

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+let throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(event => {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+  }, 1000),
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).catch(function (error) {
+  switch (error.name) {
+    case 'RangeError':
+      console.log('Invaild time!');
+      break;
+
+    default:
+      console.log('Unkown error!');
+      break;
+  }
+});
